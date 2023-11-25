64f3d40d15e8a1e0d1f59ce57236bbcf48ef69ed
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-11-25 01:23:41 +0100

    Update linux-runbook-ai.py

--- linux-runbook-ai.py
+++ linux-runbook-ai.py
@@ -3,6 +3,12 @@ from openai import OpenAI
 import streamlit as st
 
 with st.sidebar:
+
+    
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/kodesam/linux-runbook-ai.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     instruction = "code in linux scripting"
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
