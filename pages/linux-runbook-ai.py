4a631c447b26e2fc9dd7dbaa8e2581e5820eff04
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-11-25 01:24:53 +0100

    Update and rename linux-runbook-ai.py to pages/linux-runbook-ai.py

--- pages/linux-runbook-ai.py
+++ pages/linux-runbook-ai.py
@@ -6,7 +6,7 @@ with st.sidebar:
 
     
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/kodesam/i-Runbook-AI/blob/main/ansible-ai-runbook.py)"
+    "[View the source code](https://github.com/kodesam/i-Runbook-AI/blob/main/pages/ansible-ai-runbook.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
     
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
