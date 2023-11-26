b9a1cf2fd95abcb6452c82fa8d139ad777ad468f
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-11-26 22:33:48 +0100

    Update Zchat_with_user_feedback.py

--- pages/Zchat_with_user_feedback.py
+++ pages/Zchat_with_user_feedback.py
@@ -9,7 +9,7 @@ with st.sidebar:
 
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("📝 Chat with feedback (Trubrics)")
+st.title("📝 Chat with Blue-runbook and give feedback ")
 
 
 
