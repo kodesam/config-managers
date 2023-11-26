227b0ab25d47bb02b9fb38efd0c14813ed0782ac
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-11-26 22:35:11 +0100

    Update Linux-RunBook-Ai.py

--- pages/Linux-RunBook-Ai.py
+++ pages/Linux-RunBook-Ai.py
@@ -33,7 +33,7 @@ with st.sidebar:
 
  # st.title("💬 iRunBook-AI")
 
-st.caption("🚀 X-runBook for ansible code generation powered by OpenAI LLM")
+st.caption("🚀 Blue-runBook for ansible code generation powered by OpenAI LLM")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can assist you on linux scripting ?"}]
 
