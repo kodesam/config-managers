a396d06aa0e82589d164694789d44b8cdcbae39f
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-12-02 00:48:45 +0100

    Update feature.py

--- feature.py
+++ feature.py
@@ -7,9 +7,9 @@ ansible_username = st.sidebar.text_input("Ansible Username")
 ansible_password = st.sidebar.text_input("Ansible Password", type="password")
 
 # Connect to the Ansible server via SSH
-#ssh_client = paramiko.SSHClient()
-#ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#ssh_client.connect(hostname=ansible_host, username=ansible_username, password=ansible_password)
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname=ansible_host, username=ansible_username, password=ansible_password)
 
 # Execute the command on the Ansible server
 msg = "ls"
@@ -17,7 +17,7 @@ command = f"echo '{msg}' >> output.txt"  # Replace with the appropriate command 
  #stdin, stdout, stderr = ssh_client.exec_command(command)
 
 # Print the results (optional)
-# print(stdout.read().decode())
+ print(stdout.read().decode())
 
 # Close the SSH connection
-#ssh_client.close()
+ssh_client.close()
