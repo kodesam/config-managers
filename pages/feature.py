7855a820c0bf50940e727fa7eb99b6276984000e
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-12-02 00:21:15 +0100

    Create feature.py

---
+++ pages/feature.py
@@ -0,0 +1,22 @@
+import paramiko
+import streamlit as st
+
+# Get the Ansible server connection details from the sidebar input fields
+ansible_host = st.sidebar.text_input("Ansible Host")
+ansible_username = st.sidebar.text_input("Ansible Username")
+ansible_password = st.sidebar.text_input("Ansible Password", type="password")
+
+# Connect to the Ansible server via SSH
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname=ansible_host, username=ansible_username, password=ansible_password)
+
+# Execute the command on the Ansible server
+command = f"echo '{msg}' >> output.txt"  # Replace with the appropriate command to run on the server
+stdin, stdout, stderr = ssh_client.exec_command(command)
+
+# Print the results (optional)
+print(stdout.read().decode())
+
+# Close the SSH connection
+ssh_client.close()
