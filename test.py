0e281503b968b8b04372cbb2f519930d1adf8569
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-12-22 23:51:03 +0000

    test v1

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from github import Github, GithubException
 from openai import OpenAI
-import streamlit as st
 
 # Get available models
-available_models = ['gpt-3.5-turbo-16k-0613','gpt-3.5-turbo-16k-1106','gpt-3.5', 'gpt-3.0']
+available_models = ['gpt-3.5-turbo-16k-0613', 'gpt-3.5-turbo-16k-1106', 'gpt-3.5-turbo', 'gpt-3.5', 'gpt-3.0']
 
 title_style = (
     "color: blue;"
@@ -17,7 +16,8 @@ st.markdown(f"<h1 style='{title_style}'>💬 BlueRunBook-AI</h1>", unsafe_allow_
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     models = st.multiselect("Select Models", available_models, default=available_models)
-    instructions = st.text_area("Instructions", height=200)
+    instruction_1 = "code in ansible Playbook"
+    instruction_2 = st.text_area("Additional Instruction", key="additional_instruction", height=200)
     st.title("💬 BlueRunBook-AI")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 
@@ -26,9 +26,7 @@ with st.sidebar:
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 
-
 st.caption("🚀 Blue-runBook for ansible code generation powered by OpenAI LLM")
-
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can assist you on ansible code ?"}]
@@ -44,20 +42,69 @@ if prompt := st.chat_input():
     client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    
-    # Split the instructions by lines
-    instruction_lines = instructions.split("\n")
-    
-    # Add each instruction separately to the messages
-    for instruction in instruction_lines:
-        st.session_state.messages.append({"role": "assistant", "content": instruction})
-    
+
+    # Include the instructions in the conversation
+    st.session_state.messages.append({"role": "assistant", "content": instruction_1})
+    st.session_state.messages.append({"role": "assistant", "content": instruction_2})
+    #instruction = "code in ansible Playbook"
+
     # Include the instructions in the API call
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=st.session_state.messages,
     )
-    
+
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
+    
+    # Assuming you have a GitHub personal access token
+    github_token = "github_pat_11ATKQHNY0qL"
+    repo_owner = "kodesam"
+    repo_name = "collection-i-runbooks"
+    base_filename = prompt
+
+    try:
+        # Your existing code here
+
+        # Specify the folder path in the repository
+        folder_path = "ansible"
+        filename = f"{folder_path}/{base_filename}.yaml"
+
+        # Create a connection to the GitHub repository
+        g = Github(github_token)
+        repo = g.get_repo(f"{repo_owner}/{repo_name}")
+
+        # Check if the file already exists in the folder
+        file_path = f"{repo_owner}/{repo_name}/{filename}"
+        file_exists = True
+
+        try:
+            repo.get_contents(file_path)
+        except GithubException as e:
+            if e.status == 404:
+                file_exists = False
+            else:
+                raise
+
+        if not file_exists:
+            # Create or update the file in the repository
+            content = msg
+            commit_message = f"Create {filename}"
+            repo.create_file(filename, commit_message, content)
+            print(f"File '{filename}' created successfully in the GitHub repository.")
+        else:
+            print(f"File '{filename}' already exists in the GitHub repository. Skipping creation.")
+
+    except AssertionError as e:
+        # Handle the AssertionError
+        st.error("An error occurred while creating the file. Please try again later.")
+    except GithubException as e:
+        # Handle the GitHub exception
+        st.error("File already exists in the GitHub Repository folder.")
+    except NameError as e:
+        # Handle the NameError
+        st.error("An error occurred with a variable name. Please check your code and try again.")
+    except Exception as e:
+        # Handle other exceptions
+        st.error("An unexpected error occurred. Please try again later.")
