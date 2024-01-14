dcce3b26860ab4be0b1855c0e99ec731225dbc38
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2024-01-14 14:55:27 +0000

    Feature-v1 token updated

--- Pages/Feature-v1.py
+++ Pages/Feature-v1.py
@@ -56,7 +56,7 @@ with st.sidebar:
     
     instruction_1 = st.selectbox("Select Module", module)
     temperature = st.slider("Temperature", min_value=0.0, max_value=1.0, value=0.5, step=0.1)
-    max_tokens = st.number_input("Max Tokens", min_value=1, max_value=2048, value=50)
+    #max_tokens = st.number_input("Max Tokens", min_value=1, max_value=2048, value=50)
     top_p = st.slider("Top-p", min_value=0.1, max_value=1.0, value=0.9, step=0.1)
     #instruction_2 = st.text_area("Additional Instruction", key="additional_instruction", height=200)
     #st.title("💬 BlueRunBook-AI")
@@ -94,7 +94,7 @@ if prompt := st.chat_input():
         model="gpt-3.5-turbo",
         messages=st.session_state.messages,
         temperature=temperature,
-        max_tokens=max_tokens,
+        #max_tokens=max_tokens,
         top_p=top_p
     )
 
