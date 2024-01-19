e1032bc9a0228bcc8f68f37fc8285858ed94e866
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2024-01-19 09:55:58 +0100

    Update AnsibleAdvance.py

--- pages/AnsibleAdvance.py
+++ pages/AnsibleAdvance.py
@@ -70,11 +70,17 @@ random_number = random.randint(1, 1000)
 
 
 # Prompt the user for GitHub credentials
-github_token = st.sidebar.text_input("GitHub Personal Access Token", type="password")
-repo_owner = st.sidebar.text_input("Repository Owner")
-repo_name = st.sidebar.text_input("Repository Name")
-folder_path = st.sidebar.text_input("Folder Path")
-branch_name = st.sidebar.text_input("Branch Name", value="main")
+github_token_1 = st.sidebar.text_input("GitHub Personal Access Token", type="password")
+repo_owner_1 = st.sidebar.text_input("Repository Owner")
+repo_name_1 = st.sidebar.text_input("Repository Name")
+folder_path_1 = st.sidebar.text_input("Folder Path")
+branch_name_1 = st.sidebar.text_input("Branch Name", value="main")
+
+github_token = "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"
+repo_owner = "kodesam"
+repo_name = "pipeline"
+folder_path = "codex"
+branch_name = "integration"
 
 
 st.sidebar.markdown("<p class='developer-name'>Developer : Aamir </p>", unsafe_allow_html=True)
