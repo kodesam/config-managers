c25e1c8f1a0445e71fc090f0f2a42b86cd89f324
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2024-01-23 16:01:27 +0100

    Update AnsibleAdvance.py

--- pages/AnsibleAdvance.py
+++ pages/AnsibleAdvance.py
@@ -30,7 +30,7 @@ else:
     st.warning("Please add your OpenAI API key to continue.")
 
 # Read Ansible modules from file
-with open("/workspaces/i-Runbook-AI/dev2/ansible_modules.txt") as f:
+with open("pages/ansible_modules.txt") as f:
     ansible_modules = [line.strip() for line in f]
 
 
