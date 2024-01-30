8736cca566e751f6aaede42fba2534885f2b9e39
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2024-01-30 19:15:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from hashlib import sha256
 
 def check_password(stored_password, user_password):
     if stored_password == user_password:
@@ -7,27 +8,39 @@ def check_password(stored_password, user_password):
         return False
 
 def hash_pass(password):
-    return password # replace with a real hashing function if desired
+    return sha256(password.encode('utf-8')).hexdigest()
 
 def main():
-    st.title("My App")
-
-    menu = ["Login","Home","Profile"] 
+    stored_password = 'yourhashedpassword' # replace with any hashed password
+    menu = ["Login","HomePage"] 
     choice = st.sidebar.selectbox("Menu",menu)
 
     if choice == "Login":
-        username = st.sidebar.text_input("User Name")
+        st.subheader("Login Section")
+        username = st.sidebar.text_input("User_Name")
         password = st.sidebar.text_input("Password",type='password')
+        hashed_password = hash_pass(password)
         
         if st.sidebar.button("Login"):
-            hash_pass(password) # hash the password entered by the user
-            result = check_password('stored_password', password) # replace 'stored_password' with your hash password
+            result = check_password(stored_password, hashed_password)
 
             if result:
-                st.success("Logged In Successfully")
-                # your app after login
+                st.success("Logged In as {}".format(username))
+                task = st.selectbox("Task",["Add_Post","Manage_Blog","Analytics","Profiles"])
+                if task == "Add_Post":
+                    st.subheader("Add Your Blog Post")
+                elif task == "Manage_Blog":
+                    st.subheader("Manage Your Posts")
+                elif task == "Analytics":
+                    st.subheader("Analytics")
+                elif task == "Profiles":
+                    st.subheader("User Profiles")
+            
             else:
-                st.warning("Incorrect Username/Password")
+                st.warning("Invalid Username/Password")
+    elif choice == "HomePage":
+        st.subheader("Home")
+        st.info("This is a HomePage")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
